a67166a19f2c7ad6f4182e8caf4a34d1037b5855
Steve Yen <steve.yen@gmail.com>
2014-11-01 09:16:41 -0700

    initial API

---
+++ cbdatasource.go
@@ -0,0 +1,150 @@
+//  Copyright (c) 2014 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+package cbdatasource
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type BucketDataSource interface {
+	Start() error
+	Close() error
+}
+
+type bucketDataSource struct {
+	serverURLs []string
+	bucketName string
+	bucketUUID string
+	vbucketIds []uint16
+	authFunc   AuthFunc
+	receiver   Receiver
+	options    map[string]float32
+
+	m         sync.Mutex
+	isRunning bool
+}
+
+type AuthFunc func(kind string, challenge []byte) (response []byte, err error)
+
+type Receiver interface {
+	GetMetaData() ([]byte, error)
+	SetMetaData([]byte) error
+	OnDocUpdate()
+	OnDocDelete()
+	GetSeqNum()
+	Snapshot()
+	Rollback()
+}
+
+func NewBucketDataSource(serverURLs []string,
+	bucketName, bucketUUID string,
+	vbucketIds []uint16, authFunc AuthFunc,
+	receiver Receiver, options map[string]float32) (BucketDataSource, error) {
+	if len(serverURLs) < 1 {
+		return nil, fmt.Errorf("missing at least 1 serverURL")
+	}
+	if bucketName == "" {
+		return nil, fmt.Errorf("missing bucketName")
+	}
+	if receiver == nil {
+		return nil, fmt.Errorf("missing receiver")
+	}
+	if options == nil {
+		options = map[string]float32{}
+	}
+	return &bucketDataSource{
+		serverURLs: serverURLs,
+		bucketName: bucketName,
+		bucketUUID: bucketUUID,
+		vbucketIds: vbucketIds,
+		authFunc:   authFunc,
+		receiver:   receiver,
+		options:    options,
+	}, nil
+}
+
+const LOOP_SLEEP_MAX_MS = 10000
+const LOOP_SLEEP_INIT_MS = 100
+const LOOP_BACKOFF_FACTOR = 1.5
+
+func (d *bucketDataSource) Start() error {
+	d.m.Lock()
+	defer d.m.Unlock()
+
+	if d.isRunning {
+		return fmt.Errorf("already running")
+	}
+	d.isRunning = true
+
+	sleepMaxMS, exists := d.options["sleepMaxMS"]
+	if !exists {
+		sleepMaxMS = LOOP_SLEEP_MAX_MS
+	}
+	sleepInitMS, exists := d.options["sleepInitMS"]
+	if !exists {
+		sleepInitMS = LOOP_SLEEP_INIT_MS
+	}
+	backoffFactor, exists := d.options["backoffFactor"]
+	if !exists {
+		backoffFactor = LOOP_BACKOFF_FACTOR
+	}
+
+	go ExponentialBackoffLoop("bucketDataSource.run",
+		func() int { return d.run() },
+		int(sleepInitMS), backoffFactor, int(sleepMaxMS))
+
+	return nil
+}
+
+func (d *bucketDataSource) run() int {
+	return -1
+}
+
+func (d *bucketDataSource) Close() error {
+	return nil
+}
+
+// Calls f() in a loop, sleeping in an exponential backoff if needed.
+// The provided f() function should return < 0 to stop the loop; >= 0
+// to continue the loop, where > 0 means there was progress which
+// allows an immediate retry of f() with no sleeping.  A return of < 0
+// is useful when f() will never make any future progress.
+func ExponentialBackoffLoop(name string,
+	f func() int,
+	startSleepMS int,
+	backoffFactor float32,
+	maxSleepMS int) {
+	nextSleepMS := startSleepMS
+	for {
+		progress := f()
+		if progress < 0 {
+			return
+		}
+		if progress > 0 {
+			// When there was some progress, we can reset nextSleepMS.
+			// log.Printf("backoff: %s, progress: %d", name, progress)
+			nextSleepMS = startSleepMS
+		} else {
+			// If zero progress was made this cycle, then sleep.
+			// log.Printf("backoff: %s, sleep: %d (ms)", name, nextSleepMS)
+			time.Sleep(time.Duration(nextSleepMS) * time.Millisecond)
+
+			// Increase nextSleepMS in case next time also has 0 progress.
+			nextSleepMS = int(float32(nextSleepMS) * backoffFactor)
+			if nextSleepMS > maxSleepMS {
+				nextSleepMS = maxSleepMS
+			}
+		}
+	}
+}
